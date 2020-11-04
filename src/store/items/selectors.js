8fe27d31bb90187c07a6e90827a1e8c1bf7744a8
Kruxk <reinko.brink@gmail.com>
2020-11-04 19:38:58 +0100

    added selectors for fridge/pantry items

--- src/store/items/selectors.js
+++ src/store/items/selectors.js
@@ -1,3 +1,7 @@
 export const selectItems = state => state.items
 
 export const selectItemById = id => state => state.items.find(item => item.id)
+
+export const selectFridgeItems = state => state.items.filter(item => item.location === 'fridge')
+
+export const selectPantryItems = state => state.items.filter(item => item.location === 'pantry')
