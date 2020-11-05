a9a8bd7b893c200f97dcd8915639bc9f6e65c06f
tom <tomvosmusic@gmail.com>
2020-11-05 20:14:39 +0100

    created recipesPage and added it to App. The actions thing is still part of the refresh fix, but forgot to to earlier commit

---
+++ src/pages/Recipes/RecipesPage.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react'
+import Axios from 'axios'
+import RecipeCard from './RecipeCard';
+import {useSelector} from 'react-redux'
+import { selectItems } from "../../store/items/selectors";
+
+export default function RecipesPage() {
+  const [recipes, setRecipes] = useState([])
+  const items = useSelector(selectItems);
+
+  const unusedItems = items.filter(item => {
+    if (item.used === false && item.wasted === false) {
+      return item
+    }
+  })
+  const itemsByDate = unusedItems.sort((itemA, itemB) => new Date(itemA.expirationDate) - new Date(itemB.expirationDate))
+  
+
+  const ingredient = itemsByDate[0] ? itemsByDate[0].name : null
+
+  console.log({ingredient})
+
+  const recipesURL = `https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?i=${ingredient},`
+
+  const getRecipes = async () => {
+    try {
+      const response = await Axios.get(recipesURL)
+      setRecipes(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  recipes ? console.log("recipes:", recipes) : console.log("finding recipes")
+
+  useEffect(() => {
+    getRecipes()
+  }, [ingredient])
+
+
+
+  return (
+    <div>
+      <h2>{ingredient} expiring soon. Here are 10 suggested recipes using {ingredient}:</h2>
+      {recipes.results ? recipes.results.map((recipe) => {
+          return (
+            <div key={recipe.href}>
+              <RecipeCard recipe={recipe}/>
+            </div>
+          )}) : "Loading..."
+      }
+    </div>
+  )
+}
